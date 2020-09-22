aa6410e17a152f980253151f63657e267c14119f
brian <brianra2nil@hotmail.com>
2020-09-22 11:02:33 -0700

    setting up routes folder

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router()
 
-// connect your routes files here...
+router.use('/api', require('./postRoutes.js'))
+router.use('/api', require('./userRoutes.js'))
+router.use('/api', require('./commentRoutes.js'))
+
 
 module.exports = router
