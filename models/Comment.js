0042db22e9459cef062cafaa4e82ad435e4e0055
Devin Castro <dcrocker21@gmail.com>
2020-09-22 12:03:46 -0700

    user authenication and authenticated posts

---
+++ models/Comment.js
@@ -0,0 +1,18 @@
+const { model, Schema } = require('mongoose')
+
+const Comment = new Schema({
+  text: {
+    type: String,
+    required: true
+  },
+  likes: {
+    type: Number,
+    required: true
+  },
+  post: {
+    type: Schema.Types.ObjectId,
+    ref: 'Post'
+  }
+}, { timestamps: true })
+
+module.exports = model('Comment', Comment)
---
+++ models/User.js
@@ -0,0 +1,30 @@
+const { model, Schema } = require('mongoose')
+
+const User = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  genre: {
+    type: String,
+    required: false
+  },
+  posts: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Post'
+  }]
+}, { timestamps: true })
+
+User.plugin(require('passport-local-mongoose'))
+
+module.exports = model('User', User)
