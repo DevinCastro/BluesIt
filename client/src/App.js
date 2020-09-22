05f27c89883bd554ac8663c4b7dd0a0ca26b2ae5
Devin Castro <dcrocker21@gmail.com>
2020-09-22 13:46:37 -0700

    router set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+
 
 const App = () => {
   return (
-    <h1>Hello World!</h1>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react'
+// import API from '../../utils/API'
+
+const Home = () => {
+
+  return (
+    <>
+      <h1>Home page</h1>
+    </>
+  )
+}
+
+export default Home
