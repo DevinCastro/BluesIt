230db7ae9411e75cbeab0ddcbe077dec0c8de3ec
brian <brianra2nil@hotmail.com>
2020-10-01 00:04:01 -0700

    commenticon

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -14,6 +14,7 @@ import axios from 'axios'
 import { PromiseProvider } from 'mongoose';
 import Moment from 'react-moment';
 import './Post.css'
+import CommIcon from '../CommIcon/CommIcon';
 
 
 const Post = props => {
@@ -36,7 +37,7 @@ const Post = props => {
                 <CardTitle  >Post by: {props.username}</CardTitle>
               </Col>
               <Col className='left'>
-                <CardText >{props.commentNum} comments</CardText>
+                <CardText ><CommIcon/>  {props.commentNum}  comments</CardText>
               </Col>
               <Col className='left'>
                 <CardText >Posted on: <Moment format="MM/DD/YY h:mm a">{props.date}</Moment></CardText>
